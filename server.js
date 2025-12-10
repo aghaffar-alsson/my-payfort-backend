5ff12262b4bf6052ea4068d9246d976a8065d868
aghaffar-alsson <144892025+aghaffar-alsson@users.noreply.github.com>
2025-12-10 12:05:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -57,14 +57,16 @@ const sqlConfig = {
 
 
 app.use("/public", express.static(path.join(process.cwd(), "public"))); // serve files
-//Configure NODEMAILER
-// const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: 'fees@alsson.com',
-//     pass: 'gwwowluzlabnfyqw',
-//   },
-// });
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,   // secure SSL port
+  secure: true,
+  auth: {
+    user: process.env.SMTP_USER, // example: fees@alsson.com
+    pass: process.env.SMTP_PASS, // app password
+  },
+});
 
 // ---------- CREATE SIGNATURE ----------
 function createSignature(params) {
@@ -444,6 +446,7 @@ app.post("/payfort-callback", handlePayfortCallback);
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 
 
