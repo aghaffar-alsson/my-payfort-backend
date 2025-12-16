12155248334aa0dde94f01fc3afa6e1f2968f9aa
aghaffar-alsson <144892025+aghaffar-alsson@users.noreply.github.com>
2025-12-16 17:45:19 +0200

    Refactor server.js to improve structure and readability

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ const transporter = nodemailer.createTransport({
 //**********SWITCH PAYFORT CREDENTIALS ACCORDING TO THE SCHOOL ID*******
 function getMerchantCredentials(schoolId) {
   switch (schoolId) {
-    case 1:
+    case "1":
       return {
         merchant_identifier: process.env.AM_Merchant_Identifier,
         access_code: process.env.AM_Access_Code,
@@ -68,7 +68,7 @@ function getMerchantCredentials(schoolId) {
         response_phrase: process.env.AM_ResponsePhrase,
       };
 
-    case 2:
+    case "2":
       return {
         merchant_identifier: process.env.BR_Merchant_Identifier,
         access_code: process.env.BR_Access_Code,
@@ -351,6 +351,7 @@ app.post("/payfort-callback", handlePayfortCallback);
 // ---------- START SERVER ----------
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 
 
